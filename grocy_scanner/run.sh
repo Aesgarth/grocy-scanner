c8beb27ae74e9b3cc74c55a9ee85a25df9fedc0f
Aesgarth <getflair@gmail.com>
2025-01-18 21:53:52 +0000

    Update run.sh

--- grocy_scanner/run.sh
+++ grocy_scanner/run.sh
@@ -4,4 +4,5 @@ echo "Starting Grocy Item Scanner Addon..."
 echo "Initializing Flask server..."
 
 # Start Flask server
-exec python3 /app/app.py
+#exec python3 /app/app.py
+exec gunicorn -b 0.0.0.0:3456 app:app
